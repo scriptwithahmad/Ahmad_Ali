05b0b708e4ac8f507556307f89d9837ca778522a
Ahmad <scriptwithahmad@gmail.com>
2024-07-15 20:05:07 +0500

    useState Cases & useEffect Api fetching

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+
 
 const Index = () => {
+
+  const [data,setData] = useState([])
+  console.log(data)
+
+  const getData = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+      const jsonData = await res.json();
+
+      setData(jsonData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-        <h2>hey</h2>
-    </div>
-  )
-}
+      <h2>hey</h2>
 
-export default Index
+    {data?.map((v,i) => {
+      return(
+        <h1> {v?.company?.name} </h1>
+      )
+    })}
+
+    </div>
+  );
+};
+
+export default Index;
--- pages/components/Faq.jsx
+++ pages/components/Faq.jsx
@@ -1,27 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Faq = () => {
   const [value, setValue] = useState(0);
-  console.log(value);
+  const [changed, setChanged] = useState(false);
 
   return (
     <div className="p-8">
       <h1>Faq's</h1>
 
-      <button
-        onClick={() => setValue(value + 1)}
-        className="bg-blue-600 px-4 py-1 rounded-md text-white text-2xl"
-      >
-        +
-      </button>
-      <span className="p-4 text-2xl">{value}</span>
-      <button
-        onClick={() => setValue(value - 1)}
-        disabled={ value <= 0  }
-        className="bg-blue-600 disabled:bg-blue-300 px-4 py-1 rounded-md text-white text-2xl"
-      >
-        -
-      </button>
+      {/* Use Cases of UseState -------------- */}
+      {/* case: 01 */}
+      <div className="py-8">
+        <button
+          onClick={() => setValue(value - 1)}
+          disabled={value <= 0}
+          className="bg-blue-600 disabled:bg-blue-300 px-4 py-1 rounded-md text-white text-2xl"
+        >
+          -
+        </button>
+        <span className="p-4 text-2xl">{value}</span>
+        <button
+          onClick={() => setValue(value + 1)}
+          className="bg-blue-600 px-4 py-1 rounded-md text-white text-2xl"
+        >
+          +
+        </button>
+      </div>
+      {/* case: 02 */}
+      <div>
+        <button
+          onClick={() => setChanged(!changed)}
+          className="bg-gray-600 px-4 py-1 rounded-md text-white text-2xl mb-4"
+        >
+          {" "}
+          click to change the color of Div
+        </button>
+
+        <div
+          className={`border rounded-lg p-4
+
+          ${changed && "bg-red-100 text-red-600"}
+
+        `}
+        >
+          <h3>
+            The Style of this div should changed when i click on the button!
+          </h3>
+        </div>
+      </div>
+
+      {/* useEffect */}
     </div>
   );
 };
