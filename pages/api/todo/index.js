aa88d71d115778f0b9da3a510a0c2c2d748a9da5
Ahmad <scriptwithahmad@gmail.com>
2024-08-02 18:44:48 +0500

    todo api

--- pages/api/todo/index.js
+++ pages/api/todo/index.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
 
         res.status(200).json({
           success: true,
-          message: getTodo  ,
+          message: getTodo,
         });
       } catch (error) {
         console.log(error);
